cfb9e0aef5410e9a11ac3b1ddbe3d3a35707cfd6
clnoll <noll.catherine@gmail.com>
2014-09-25 02:36:13 -0700

    setup with angular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require foundation
+//= require angular
+//= require angular-resource
+//= require angular-route
+//= require_tree ./angular-foundation
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Breeze::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'application#index'
+  get '*path' => 'application#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
