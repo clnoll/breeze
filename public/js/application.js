7d2dc90858d88dee1f442c9fc6f0f25f4b3da741
clnoll <noll.catherine@gmail.com>
2014-09-28 20:48:21 -0700

    formatting and calculations

--- public/js/application.js
+++ public/js/application.js
@@ -18,7 +18,7 @@
            insurance: 125,
            lease: 125,
            hoursDrive: 30,
-           hoursTask: 30,
+           hoursTask: 0,
            mileage: 135,
            shift: 1,
            vehicle: 1,
@@ -27,19 +27,19 @@
            selectedShift: 'Weekday',
            selectedVehicle: 'Standard',
            selectedAgency: 'Uber',
-           selectedRegion: 'SanFrancisco'
+           selectedRegion: 'San Francisco'
        };
 
        $scope.selectOptions = {
            shifts: ['Weeknight', 'Weekend', 'Weekday'],
            vehicles: ['Standard', 'Mid-range', 'Premium'],
            agencies: ['Uber', 'Lyft', 'SpoonRocket', 'TaskRabbit'],
-           regions: ['SanFrancisco', 'EastBay', 'SouthBay', 'Peninsula']
+           regions: ['San Francisco', 'East Bay', 'South Bay', 'Peninsula']
        };
 
       // Calculate the weekly cost of gas based on estimated hours of driving per week.  Assumes average of 30 mph speed, 40 mpg for hybrids, $4.50/gallon gas
        var mileageFxn = function() {
-           return (($scope.values.hoursDrive * 30) / 40) * 4.5
+           return Math.floor((($scope.values.hoursDrive * 30) / 40) * 4.5)
        };
 
        // Set relative values of shifts
@@ -58,9 +58,9 @@
        var vehicleFxn = function() {
           var selectedVehicle = $scope.values.selectedVehicle;
            if (selectedVehicle === "Premium") {
-               return 2
-           } else if (selectedVehicle === "Mid-range") {
                return 1.5
+           } else if (selectedVehicle === "Mid-range") {
+               return 1.25
            } else if (selectedVehicle === "Standard") {
                return 1
            }
@@ -69,11 +69,11 @@
        // Set relative values of region
        var regionFxn = function() {
           var selectedRegion = $scope.values.selectedRegion;
-           if (selectedRegion === "SanFrancisco") {
+           if (selectedRegion === "San Francisco") {
                return 1.5
-           } else if (selectedRegion === "EastBay") {
+           } else if (selectedRegion === "East Bay") {
                return 1
-           } else if (selectedRegion === "SouthBay") {
+           } else if (selectedRegion === "South Bay") {
                return 1.25
            } else if (selectedRegion === "Peninsula") {
                return 1.25
@@ -84,13 +84,13 @@
        var agencyFxn = function() {
           var selectedAgency = $scope.values.selectedAgency;
            if (selectedAgency === "Uber") {
-               return 40
-           } else if (selectedAgency === "Lyft") {
                return 35
-           } else if (selectedAgency === "SpoonRocket") {
-               return 25
-           } else if (selectedAgency === "TaskRabbit") {
+           } else if (selectedAgency === "Lyft") {
                return 30
+           } else if (selectedAgency === "SpoonRocket") {
+               return 20
+           } else if (selectedAgency === "TaskRabbit") {
+               return 25
            }
        };
 
@@ -112,18 +112,13 @@
 
            // Calculate the rate estimate
            var rate = ((multipliers - costsAnnual)/(52 * 40))
-           console.log(rate)
-           console.log((multipliers - costsAnnual)/(52*40))
-           console.log("SHIFT From AddVals: " + $scope.values.shift)
-           console.log("REGION From AddVals: " + $scope.values.region)
            return Math.floor(rate)
        };
 
        // Assign estimate to the scope by watching changes to the $scope.values object
        $scope.$watchCollection("values", function(newVal, oldVal, scope) {
-            console.log("SHIFT" + scope.values.shift);
-            console.log("REGION" + scope.values.region);
            scope.values.estimate = addVals();
+           scope.values.mileage = mileageFxn();
        })
 
        // Formatting for scale bar
